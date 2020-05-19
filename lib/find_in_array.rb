baf6357b49baaa6499302495a1ef4a42bd071bfe
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 01:41:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   
   array.each do |index|
     if index==value_to_find
-      return value_to_find
+      return index
     else
       return nill
       end
